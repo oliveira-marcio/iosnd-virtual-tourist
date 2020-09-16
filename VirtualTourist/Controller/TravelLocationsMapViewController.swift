69cb24b8dcb64087929376c722fb8b22a85682c5
oliveira-marcio <emaildomso@gmail.com>
2020-09-16 09:02:55 +0100

    feat: Load static map pin with click event

--- VirtualTourist/Controller/TravelLocationsMapViewController.swift
+++ VirtualTourist/Controller/TravelLocationsMapViewController.swift
@@ -7,18 +7,61 @@
 //
 
 import UIKit
+import MapKit
 
-class TravelLocationsMapViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
 
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadMapAnnotations()
     }
 
-    @IBAction func showPhotoAlbum(_ sender: Any) {
+    private func loadMapAnnotations() {
+        mapView.delegate = self
+
+        let lat = CLLocationDegrees(38.714761)
+        let long = CLLocationDegrees(-9.1568121)
+        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+
+        var annotations = [MKPointAnnotation]()
+        annotations.append(annotation)
+
+        let region = MKCoordinateRegion( center: coordinate, latitudinalMeters: CLLocationDistance(exactly: 5000)!, longitudinalMeters: CLLocationDistance(exactly: 5000)!)
+
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.showAnnotations(annotations, animated: true)
+        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+    }
+
+    // MARK: - Map View Delegate
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        let reuseId = "pin"
+
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = false
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+
+        return pinView
+    }
+
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         performSegue(withIdentifier: "showPhotoAlbum", sender: nil)
+        mapView.selectedAnnotations = []
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
