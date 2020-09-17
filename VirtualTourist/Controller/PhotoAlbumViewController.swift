74c0379abb708c99dfc43e7dd0a5dcd665dbaae5
oliveira-marcio <emaildomso@gmail.com>
2020-09-17 12:52:46 +0100

    feat: Add map view with selected pin on photo album controller

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -9,16 +9,48 @@
 import UIKit
 import MapKit
 
-class PhotoAlbumViewController: UIViewController {
+class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+
     var selectedCoordinate: CLLocationCoordinate2D!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.delegate = self
+        loadMapResults()
+
         label.text = "\(selectedCoordinate.latitude), \(selectedCoordinate.longitude)"
     }
     
+    private func loadMapResults() {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = selectedCoordinate
+
+        self.mapView.showAnnotations([annotation], animated: false)
+    }
+
+    // MARK: - Map View Delegate
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        let reuseId = "pin"
+
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = false
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+
+        return pinView
+    }
 
 }
